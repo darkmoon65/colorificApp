faf711f50ee5b1753287a0450dba4669bd0c8680
cesar Santillana <icetom6@hotmail.com>
2023-07-02 18:34:28 -0500

    input

--- src/FirstExploreScreen.js
+++ src/FirstExploreScreen.js
@@ -1,12 +1,28 @@
 import { Audio } from 'expo-av';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Image, TouchableHighlight, ImageBackground, Text} from 'react-native';
+import { StyleSheet, View, Image, TouchableHighlight, ImageBackground, Text, TextInput} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import NavBar, { NavButton, NavButtonText, NavTitle } from 'react-native-nav'
 
 const FirstExploreScreen = ({navigation}) => {
     const [sound, setSound] = React.useState();
-  
+    // Data input
+    const [item1, setItem1] = useState("");
+    const [item2, setItem2] = useState("");
+    const [item3, setItem3] = useState("");
+    const [item4, setItem4] = useState("");
+    const [item5, setItem5] = useState("");
+    const [item6, setItem6] = useState("");
+    const [item7, setItem7] = useState("");
+    const [item8, setItem8] = useState("");
+    const [item9, setItem9] = useState("");
+
+    let checkWrite = async (last) => {
+      var escrito = item1 + item2 + item3 + item4 +item5 + item6 +item7 + item8 + item9 + last
+
+      console.log(escrito)
+      if(escrito == "vermillion" ) console.log("Good y aparece modal")
+    }
     useEffect(() => {
       return sound
         ? () => {
@@ -47,9 +63,50 @@ const FirstExploreScreen = ({navigation}) => {
               <TouchableHighlight style={styles.submit} onPress={() => navigation.navigate('FirstExplore')} > 
                 <Text style={styles.submitText}>VERMILLION</Text>
               </TouchableHighlight>
+              <View style={{paddingTop: 15}}>
+                <Text style={{fontSize: 25}}>Pronunciación: /vɚˈmɪl.jən/</Text>
+              </View>
+              <View style={{paddingTop: 15}}>
+                <Text style={{fontSize: 25}}>Traducción: Bermellón</Text>
+              </View>
             </View>
-            <Image source={require('../assets/triangle.png')} style={{width: 350, height: 300}}>
-            </Image>
+            <View style={styles.center}>
+              <Image source={require('../assets/vermi.png')} style={{width: '60%', height: '68%'}}>
+              </Image>
+              <View style={styles.oneLine}>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => setItem1(value)}/> 
+                </View>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => setItem2(value)}/> 
+                </View>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => setItem3(value)}/> 
+                </View>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => setItem4(value)}/> 
+                </View>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => setItem5(value)}/> 
+                </View>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => setItem6(value)}/> 
+                </View>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => setItem7(value)}/> 
+                </View>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => setItem8(value)}/> 
+                </View>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => setItem9(value)}/> 
+                </View>
+                <View> 
+                  <TextInput style={styles.input}  maxLength={1} onChangeText={(value) => checkWrite(value)}/> 
+                </View>
+              </View>
+            </View>
+
             <TouchableHighlight style={{position: 'absolute', top: 70, right: 165}}
                 onPress={ () => onPress('yellow')} > 
                 <Image source={require('../assets/sound.png')} style={{width: 30, height: 30}} />
@@ -60,6 +117,14 @@ const FirstExploreScreen = ({navigation}) => {
     );
   };
   const styles = StyleSheet.create({
+    input: {
+      backgroundColor: "white",
+      borderWidth: 1,
+      borderRadius: 5
+    },
+    oneLine:{
+      flexDirection: 'row',
+    },
     container: {
       padding: 20,
       //fontFamily: 'Arial'
